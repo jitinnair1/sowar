84d6409f5bc0dc2254c68d1d59971f64d5bd4d92
Jitin <jitin.s.nair@gmail.com>
2026-01-14 13:52:25 +0530

    clean up dev script

--- dev.ts
+++ dev.ts
@@ -6,10 +6,7 @@ await $`bun run build:dist`;
 //run watchers in parallel
 await Promise.all([
   $`bun run build:ts --watch`,
-
-  //output directly to dist during dev if that's what your server needs.
   $`bunx @tailwindcss/cli -i ./src/input.css -o ./dist/style.css --watch`,
-
   $`bun run serve`
 ]);
 
