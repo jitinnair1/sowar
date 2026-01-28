8b8b3ee5a54bc5e8f853a1d4f83e826c0712bb52
Jitin <jitin.s.nair@gmail.com>
2026-01-29 00:56:30 +0530

    wip exercise 03

---
+++ src/exercises/03_currying/index.ts
@@ -0,0 +1,15 @@
+import { Exercise } from '../../types';
+import description from "./problem.md" with { type: "text" };
+import initialCode from "./template.ml" with { type: "text" };
+
+const exercise: Exercise = {
+    id: "1.3",
+    title: "Currying",
+    description: description,
+    initialCode: initialCode,
+    validate: (output: string) => {
+      //TBD
+    }
+};
+
+export default exercise;
---
+++ src/exercises/03_currying/test.ml
@@ -0,0 +1,3 @@
+(* This code is silently appended to the user's code *)
+let () =
+  assert (average 1.0 2.0 = 1.5);
