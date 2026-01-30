eb5916e53b659d267d9de0022412ce6e7ede99b0
Jitin <jitin.s.nair@gmail.com>
2026-01-30 15:53:47 +0530

    cleaup icons

--- src/ui/icons.ts
+++ src/ui/icons.ts
@@ -1,24 +1,38 @@
 export const ICONS = {
-    /*
-    While adding a new SVG, use the currrentColor for stroke or fill values 
-    */
-    CHECK: `<svg xmlns="http://www.w3.org/2000/svg"
+  /*
+     While adding a new SVG, use the currrentColor for stroke or fill values
+     */
+  CHECK: `<svg xmlns="http://www.w3.org/2000/svg"
     width="14" height="14" viewBox="0 0 24 24" fill="none"
-    stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
-    class="text-green-500">
-    <polyline points="20 6 9 17 4 12"></polyline></svg>`,
+  stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
+  class="text-green-500">
+  <polyline points="20 6 9 17 4 12"></polyline></svg>`,
 
     LEFT_ARROW: `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-    <path d="m15 18-6-6 6-6" /></svg>`,
+  stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <path d="m15 18-6-6 6-6" /></svg>`,
 
 
     RIGHT_ARROW: `<svg width="18" height="18" style="display: block; transform: scale(-1,1)" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-    stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-    <path d="m15 18-6-6 6-6" /></svg>`,
+  stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <path d="m15 18-6-6 6-6" /></svg>`,
 
-    PLAY: `<svg fill="currentColor" width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z"/></svg>`,
+    PLAY: `<svg fill="currentColor" width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z"/>
+  </svg>`,
 
-    STOP: `<svg fill="currentColor" width="16" height="16" viewBox="-1 0 19 19" xmlns="http://www.w3.org/2000/svg" class="cf-icon-svg"><path d="M16.417 9.583A7.917 7.917 0 1 1 8.5 1.666a7.917 7.917 0 0 1 7.917 7.917zm-5.267 6.274a6.766 6.766 0 0 0 1.756-1.084L3.31 5.177a6.81 6.81 0 0 0 7.84 10.68zm3.624-3.624a6.808 6.808 0 0 0-10.68-7.84l9.596 9.596a6.77 6.77 0 0 0 1.084-1.756z"/></svg>`
+  STOP: `<svg fill="currentColor" width="16" height="16" viewBox="-1 0 19 19" xmlns="http://www.w3.org/2000/svg"
+    class="cf-icon-svg"><path d="M16.417 9.583A7.917 7.917 0 1 1 8.5 1.666a7.917 7.917 0 0 1 7.917 7.917zm-5.267 6.274a6.766 6.766 0 0 0 1.756-1.084L3.31 5.177a6.81 6.81 0 0 0 7.84 10.68zm3.624-3.624a6.808 6.808 0 0 0-10.68-7.84l9.596 9.596a6.77 6.77 0 0 0 1.084-1.756z"/>
+  </svg>`,
 
+  TRASH: `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+    width="14" height="14" viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve">
+  <path fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" d="M23,27H11c-1.1,0-2-0.9-2-2V8h16v17
+  C25,26.1,24.1,27,23,27z"/>
+  <line fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" x1="27" y1="8" x2="7" y2="8"/>
+  <path fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" d="M14,8V6c0-0.6,0.4-1,1-1h4c0.6,0,1,0.4,1,1v2"/>
+  <line fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" x1="17" y1="23" x2="17" y2="12"/>
+  <line fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" x1="21" y1="23" x2="21" y2="12"/>
+  <line fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" x1="13" y1="23" x2="13" y2="12"/>
+  </svg>`,
 };
