3a3cb6a441b7b45a07e6c983cbaaf8df7e83e5d3
Jitin <jitin.s.nair@gmail.com>
2026-01-30 15:01:47 +0530

    cleanup console out formatting

--- src/exercises/02_ints_vs_floats/test.ml
+++ src/exercises/02_ints_vs_floats/test.ml
@@ -1,7 +1,7 @@
 let () =
   let result = average 2. 5. in
   if result <> 3.5 then begin
-    Printf.printf "\nTest failed: average 2. 5. returned " ^ (string_of_float result) ^ " but expected 3.5";
+    Printf.printf "\nTest failed: average 2. 5. returned %s but expected 3.5\n\n" (string_of_float result);
     failwith "Test failed"
   end else
-    Printf.printf "\nTest passed: average 2. 5. = 3.5";;
+    Printf.printf "\nTest passed: average 2. 5. = 3.5\n";;
--- src/exercises/03_currying/test.ml
+++ src/exercises/03_currying/test.ml
@@ -2,15 +2,15 @@
 let () =
   let result1 = add_thirteen 7 in
   if result1 <> 20 then begin
-    Printf.printf "\nTest failed: add_thirteen 7 returned " ^ (string_of_int result1) ^ " but expected 20";
+    Printf.printf "\nTest failed: add_thirteen 7 returned %d but expected 20\n\n" result1;
     failwith "Test failed"
   end else
-    Printf.printf "\nTest passed: add_thirteen 7 = 20";;
+    Printf.printf "\nTest passed: add_thirteen 7 = 20\n";;
 
 let () =
   let result2 = multiply_by_17 2 in
   if result2 <> 34 then begin
-    Printf.printf "\nTest failed: multiply_by_17 2 returned " ^ (string_of_int result2) ^ " but expected 34";
+    Printf.printf "\nTest failed: multiply_by_17 2 returned %d but expected 34\n\n" result2;
     failwith "Test failed"
   end else
-    Printf.printf "\nTest passed: multiply_by_17 2 = 34";;
+    Printf.printf "\nTest passed: multiply_by_17 2 = 34\n";;
