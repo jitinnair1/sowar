5dcc4761f51b3a00b2a7d456db22e6590fbd686f
Jitin <jitin.s.nair@gmail.com>
2026-01-31 15:44:50 +0530

    thicker arrows because why not

--- src/ui/icons.ts
+++ src/ui/icons.ts
@@ -9,12 +9,12 @@ export const ICONS = {
   <polyline points="20 6 9 17 4 12"></polyline></svg>`,
 
   LEFT_ARROW: `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-  stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  stroke-width="5" stroke-linecap="round" stroke-linejoin="round">
   <path d="m15 18-6-6 6-6" /></svg>`,
 
 
   RIGHT_ARROW: `<svg width="18" height="18" style="display: block; transform: scale(-1,1)" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-  stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  stroke-width="5" stroke-linecap="round" stroke-linejoin="round">
   <path d="m15 18-6-6 6-6" /></svg>`,
 
   PLAY: `<svg fill="currentColor" width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
