b04b29d8be12e17984b48830eb956758dd9773d7
Jitin <jitin.s.nair@gmail.com>
2026-02-01 03:43:26 +0530

    refactor confetti into ui

--- src/ui/confetti.ts
+++ src/ui/confetti.ts
@@ -1,11 +1,13 @@
+import canvasConfetti from 'canvas-confetti';
+
 function randomInRange(min: number, max: number) {
     return Math.random() * (max - min) + min;
 }
 
 export function confetti() {
     canvasConfetti({
-        angle: randomInRange(55, 125),
-        spread: randomInRange(50, 70),
+        angle: randomInRange(60, 120),
+        spread: randomInRange(30, 70),
         particleCount: randomInRange(50, 100),
         origin: { y: 0.6 }
     });
