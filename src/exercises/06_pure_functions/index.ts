cec4ddef68d6773919ecefdff5ca46aca4e71455
Jitin <jitin.s.nair@gmail.com>
2026-02-08 19:00:56 +0530

    wip new exercise

---
+++ src/exercises/06_pure_functions/index.ts
@@ -0,0 +1,15 @@
+import { Exercise } from '../../core/types';
+import description from "./problem.md" with { type: "text" };
+import initialCode from "./template.ml" with { type: "text" };
+import testCode from "./test.ml" with { type: "text" };
+
+const exercise: Exercise = {
+  id: "2.2",
+  title: "Pure Functions",
+  description: description,
+  initialCode: initialCode,
+  testCode: testCode,
+  validate: () => true
+};
+
+export default exercise;
